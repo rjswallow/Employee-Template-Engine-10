3beca861f76879eb3becb52832569226b5360d95
Ralds Oril <robertjswallow@gmail.com>
2021-01-12 22:46:43 -0800

    pulled from previous repo

---
+++ app.js
@@ -0,0 +1,208 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+const arr = [];
+const idArr = [];
+const validateId = async (data) => {
+if(idArr.includes(data) === true || isNaN(data) ) {
+    return  "ID not valid, please use numerical values";
+} return true;
+
+};
+
+
+
+// Write code to use inquirer to gather information about the development team members,
+
+function init() {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'number',
+            name: 'id',
+            message: 'What is your id number?',
+            validate: validateId,
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is your office number?',
+        },
+        {
+            type: 'list',
+            name: 'new',
+            message: 'What type of employee would you like to add?',
+            choices: ['Engineer', 'Intern']
+        },
+    ]).then((answers) => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        arr.push(manager);
+        idArr.push(answers.id);
+
+        switch (answers.new) {
+            case "Engineer":
+                engineer();
+                break;
+            case "Intern":
+                intern();
+                break;
+
+
+            default:
+                break;
+        }
+    })
+};
+
+function engineer() {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is employee name?',
+        },
+        {
+            type: 'number',
+            name: 'id',
+            message: 'What is employee id number?',
+            validate: validateId,
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the employee email address?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is employee GitHub?',
+        },
+        {
+            type: 'list',
+            name: 'new',
+            message: 'What type of employee would you like to add?',
+            choices: ['Engineer', 'Intern', "none"]
+        },
+    ]).then((answers) => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        arr.push(engineer);
+        idArr.push(answers.id);
+        switch (answers.new) {
+            case "Engineer":
+                engineer();
+                break;
+            case "Intern":
+                intern();
+                break;
+            case "none":
+                fs.writeFile(outputPath, render(arr), (err) => {
+                    if (err) throw err;
+
+                });
+                break;
+
+            default:
+                break;
+        }
+    })
+}
+
+function intern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is intern name?',
+        },
+        {
+            type: 'number',
+            name: 'id',
+            message: 'What is intern id number?',
+            validate: validateId,
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the intern email address?',
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school is intern attending?',
+        },
+        {
+            type: 'list',
+            name: 'new',
+            message: 'What type of employee would you like to add?',
+            choices: ['Engineer', 'Intern', 'none']
+        },
+
+    ]).then((answers) => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        arr.push(intern);
+        idArr.push(answers.id);
+        switch (answers.new) {
+            case "Engineer":
+                engineer();
+                break;
+            case "Intern":
+                intern();
+                break;
+            case "none":
+                fs.writeFile(outputPath, render(arr), (err) => {
+                    if (err) throw err;
+
+                });
+                break;
+
+            default:
+                break;
+        }
+    })
+};
+
+
+init();
+
+
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
